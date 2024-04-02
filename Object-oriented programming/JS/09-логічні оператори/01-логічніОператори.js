313aedd0df38ba8bd79e343abec7441a403584ce
Ihor Koliasa <ihor@koliasa.com>
2024-04-02 13:01:54 +0300

    Добавив логічні оператори ні, і, якщо. Також добавив приклади цих операторів
     On branch Object-oriented-programming
     Your branch is ahead of 'origin/Object-oriented-programming' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   "Object-oriented programming/JS/09-\320\273\320\276\320\263\321\226\321\207\320\275\321\226 \320\276\320\277\320\265\321\200\320\260\321\202\320\276\321\200\320\270/01-\320\273\320\276\320\263\321\226\321\207\320\275\321\226\320\236\320\277\320\265\321\200\320\260\321\202\320\276\321\200\320\270.js"
    	new file:   "Object-oriented programming/JS/09-\320\273\320\276\320\263\321\226\321\207\320\275\321\226 \320\276\320\277\320\265\321\200\320\260\321\202\320\276\321\200\320\270/example.js"
    	new file:   "Object-oriented programming/JS/09-\320\273\320\276\320\263\321\226\321\207\320\275\321\226 \320\276\320\277\320\265\321\200\320\260\321\202\320\276\321\200\320\270/\320\260\320\261\320\276.js"
    	new file:   "Object-oriented programming/JS/09-\320\273\320\276\320\263\321\226\321\207\320\275\321\226 \320\276\320\277\320\265\321\200\320\260\321\202\320\276\321\200\320\270/\320\264\320\265\320\272\321\226\320\273\321\214\320\272\320\260 \321\226 \320\260\320\261\320\276.js"
    	new file:   "Object-oriented programming/JS/09-\320\273\320\276\320\263\321\226\321\207\320\275\321\226 \320\276\320\277\320\265\321\200\320\260\321\202\320\276\321\200\320\270/\320\275\321\226-\320\277\320\276\320\264\320\262\321\226\320\271\320\275\320\265.js"
    	new file:   "Object-oriented programming/JS/09-\320\273\320\276\320\263\321\226\321\207\320\275\321\226 \320\276\320\277\320\265\321\200\320\260\321\202\320\276\321\200\320\270/\320\275\321\226.js"
    	new file:   "Object-oriented programming/JS/09-\320\273\320\276\320\263\321\226\321\207\320\275\321\226 \320\276\320\277\320\265\321\200\320\260\321\202\320\276\321\200\320\270/\321\226.js"

---
+++ Object-oriented programming/JS/09-логічні оператори/01-логічніОператори.js
@@ -0,0 +1,4 @@
+"Ihor" || "defaultValue";
+console.log("Ihor" || "defaultValue"); // Виведе "Ihor", оскільки "Ihor" є істинним значенням (не false, null, undefined, 0 або '')
+"" || "defaultValue";
+console.log("" || "defaultValue"); // Виведе "defaultValue" тому що перше значення є хибним
---
+++ Object-oriented programming/JS/09-логічні оператори/або.js
@@ -0,0 +1,21 @@
+// Оператор || (логічне "або") використовується для вибору першого істинного (true) значення серед декількох виразів або операндів.
+// Якщо хоча б один з операндів є true, весь вираз поверне true. Якщо всі операнди є false, вираз поверне false.
+
+let isSunny = true;
+let isRaining = false;
+
+let isWeatherGood = isSunny || isRaining;
+
+console.log(isWeatherGood); // Виведе true, оскільки хоча б один з операндів (isSunny) є true
+
+// У цьому прикладі isWeatherGood буде true, оскільки isSunny має значення true. Оператор || дозволяє нам обирати перше істинне значення
+// серед isSunny та isRaining, тому весь вираз стає true.
+
+let g = "ihor";
+let h = "petro";
+
+let atLeastOneStartsWithI = g.startsWith("i") || h.startsWith("i");
+
+console.log(atLeastOneStartsWithI); // Виведе true, оскільки g починається з 'i'
+// Тут, оскільки g починається з 'i', вираз g.startsWith('i') повертає true, і ми вже знаємо,
+// що хоча б один з виразів є true, оскільки ми використовуємо оператор ||, тому результат буде true.
